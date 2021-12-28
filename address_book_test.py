75039010c8c4727e041c42f2d253fdda5f62b09e
Sunanda123-png <sunanda02@gmail.com>
2021-12-28 11:42:29 +0530

    Add: Test the delete contact method

--- address_book_test.py
+++ address_book_test.py
@@ -32,7 +32,7 @@ def test_edit_details():
 
 def test_delete_contact():
     """
-    testing delete contact method
+    test delete contact method
     :return:
     """
     address_book = AddressBook()
