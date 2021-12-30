cda9c05d4307593e40c21a2b5b41e6d8d7b788d4
Sunanda123-png <sunanda02@gmail.com>
2021-12-30 22:15:13 +0530

    Add: refactor the edit details and adding delete contact

--- address_book.py
+++ address_book.py
@@ -1,9 +1,9 @@
-import logging
-
 """
 Author:- Sunanda Shil
 Date:- 27-12-21
 """
+import logging
+
 logging.basicConfig(filename="address_book.log", filemode="w")
 
 
@@ -15,7 +15,7 @@ class Contact:
     def __init__(self, contact_dict):
         """
         created constructor for initialize the variable
-        :param contact_dict:
+        :param contact_dict: this dictionary contain all parameter like firstname,lastname
         """
         self.first_name = contact_dict.get("first_name")
         self.last_name = contact_dict.get("last_name")
@@ -25,26 +25,77 @@ class Contact:
         self.state = contact_dict.get("state")
         self.pin_no = contact_dict.get("pin_no")
 
+    def set_first_name(self, firstname):
+        """
+        setter method for firstname
+        :param firstname:
+        """
+        self.first_name = firstname
+
+    def set_last_name(self, lastname):
+        """
+        setter method for lastname
+        :param lastname:
+        """
+        self.last_name = lastname
+
+    def set_ages(self, ages):
+        """
+        setter method for age
+        :param ages:
+        """
+        self.age = ages
+
+    def set_mobile_number(self, mobile_number):
+        """
+        setter method for mobile number
+        :param mobile_number:
+        """
+        self.mobile_no = mobile_number
+
+    def set_email_id(self, email_id):
+        """
+        setter method for email
+        :param email_id:
+        """
+        self.email = email_id
+
+    def set_state(self, state_person):
+        """
+        setter method for state
+        :param state_person:
+        """
+        self.state = state_person
+
+    def set_pin(self, pin_number):
+        """
+        setter method for pin number
+        :param pin_number:
+        """
+        self.pin_no = pin_number
+
 
 class AddressBook:
     """
     created address book class for making require method
     """
-    def __init__(self):
-        self.address_list = []
+
+    def __init__(self, name):
+        self.name = name
+        self.contact_list = []
 
     def add_person(self, person_address):
         """
         Creating this method for adding person in list
         :param person_address: is a object
         """
-        self.address_list.append(person_address)
+        self.contact_list.append(person_address)
 
     def show_details(self):
         """
         for display the inputted value created show details method
         """
-        for address in self.address_list:
+        for address in self.contact_list:
             print("The firstname is :-", address.first_name)
             print("The lastname is :- ", address.last_name)
             print("Age of this person is :- ", address.age)
@@ -53,52 +104,21 @@ class AddressBook:
             print("The state of this person is :- ", address.state)
             print("Pin number is :- ", address.pin_no)
 
-    def edit_details(self):
+    def edit_contact(self, contact_names, choices, values):
         """
-        for editing the existing details created edit_details method
+        For edit the person contact this method has created
+        :param contact_names: this name will take from main
+        :param choices: choices which user want to edit the contact information
+        :param values: user inputted new value which will update in contact
+        :return: contact
         """
-        try:
-
-            for contact in self.address_list:
-                if contact.first_name == contact_name:
-                    print("""Your choice:-
-                                    1.First name
-                                    2.Last name
-                                    3.Age
-                                    4.Mobile number
-                                    5.Email
-                                    6.State
-                                    7.Pin number
-                                    """)
-                    choices = int(input("Enter your choice:- "))
-                    if choices == 1:
-                        firstname = input("Enter the new first name:- ")
-                        contact.first_name = firstname
-                    elif choices == 2:
-                        lastname = input("Enter new last name:- ")
-                        contact.last_name = lastname
-                    elif choices == 3:
-                        ages = int(input("Enter new age:- "))
-                        contact.age = ages
-                    elif choices == 4:
-                        mobile_number = int(input("Enter new mobile number:- "))
-                        contact.mobile_no = mobile_number
-                    elif choices == 5:
-                        email_id = input("Enter new email id:-")
-                        contact.email = email_id
-                    elif choices == 6:
-                        state_person = input("Enter new state:- ")
-                        contact.state = state_person
-                    elif choices == 7:
-                        pin_number = int(input("Enter new pin number:- "))
-                        contact.pin_no = pin_number
-                    else:
-                        print("Wrong choice!!!")
-                        break
-                else:
-                    print("Name not found")
-        except Exception:
-            logging.exception("Type proper value!!!")
+        for contacts in self.contact_list:
+            if contact.first_name == contact_names:
+                contact_dictionary = {1: contacts.set_first_name, 2: contacts.set_last_name, 3: contacts.set_ages,
+                                      4: contacts.set_mobile_number, 5: contacts.set_email_id,
+                                      6: contacts.set_state, 7: contacts.set_pin}
+                contact_dictionary.get(choices)(values)
+                return contact
 
     def delete_contact(self):
         """
@@ -106,17 +126,32 @@ class AddressBook:
         """
         try:
             delete_contact = input("Enter the first name you want to delete:- ")
-            for contact in self.address_list:
-                if contact.first_name == delete_contact:
-                    self.address_list.remove(contact)
+            for contacts in self.contact_list:
+                if contacts.first_name == delete_contact:
+                    self.contact_list.remove(contacts)
                 else:
                     print("Name not found")
         except Exception:
             logging.exception("Type string value!!!")
 
 
+def get_address_book(multi_address_books, address_books_name):
+    """
+
+    :param multi_address_books: for multiple address book
+    :param address_books_name: name of the address book
+    :return:
+    """
+    for address_books in multi_address_books:
+        if address_book.name == address_books_name:
+            return multi_address_books, address_books
+    address_books = AddressBook(address_books_name)
+    multi_address_books.append(address_books)
+    return multi_address_books, address_books
+
+
 if __name__ == "__main__":
-    address_book = AddressBook()
+    multi_address_book = []
     try:
         while True:
             print("""
@@ -128,6 +163,8 @@ if __name__ == "__main__":
                 """)
             choice = int(input("Enter your choice:- "))
             if choice == 1:
+                address_book_name = input("Enter the address book name:- ")
+                list_of_address_book, address_book = get_address_book(multi_address_book, address_book_name)
                 first_name = input("Enter first name:- ")
                 last_name = input("Enter last name:- ")
                 age = int(input("Enter ages:- "))
@@ -135,17 +172,44 @@ if __name__ == "__main__":
                 email = input("Enter the email id:- ")
                 state = input("Enter the state :-")
                 pin_no = int(input("Enter the pin number:- "))
+
                 contact_dict = {"first_name": first_name, "last_name": last_name, "age": age,
                                 "mobile_no": mobile_no, "email": email, "state": state, "pin_no": pin_no}
                 contact = Contact(contact_dict)
                 address_book.add_person(contact)
             elif choice == 2:
+                address_book_name = input("Enter the address book name:- ")
+                list_of_address_book, address_book = get_address_book(multi_address_book, address_book_name)
                 address_book.show_details()
             elif choice == 3:
-                contact_name = input("Enter the Firstname of person which you want to edit information:- ")
-                address_book.edit_details()
+
+                address_book_name = input("Enter the address book name:- ")
+                list_of_address_book, address_book = get_address_book(multi_address_book, address_book_name)
+                try:
+                    contact_name = input("Enter the Firstname which you want to edit info:- ")
+
+                    print("""Your choice:-
+                           1.First name
+                           2.Last name
+                           3.Age
+                           4.Mobile number
+                           5.Email
+                           6.State
+                           7.Pin number
+                           """)
+                    choices = int(input("Enter your choice:- "))
+                    value = input("Enter the value:- ")
+                    address_book.edit_contact(contact_name, choices, value)
+
+                except Exception:
+                    logging.exception("Choose proper option!!!")
             elif choice == 4:
+                address_book_name = input("Enter the address book name:- ")
+                list_of_address_book, address_book = get_address_book(multi_address_book, address_book_name)
                 address_book.delete_contact()
+            else:
+                print("Wrong choice!!!")
+                break
 
     except Exception:
         logging.exception("Enter proper value!!!")
