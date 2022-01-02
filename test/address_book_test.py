2ac8d894898397880d33cbddeea76047be9872c8
Sunanda123-png <sunanda02@gmail.com>
2022-01-02 12:13:41 +0530

    Add: testing the every method in addressbook

--- test/address_book_test.py
+++ test/address_book_test.py
@@ -5,40 +5,43 @@ def test_add_person():
     """
     testing the add_person method
     """
-    address_book = AddressBook()
+    address_book = AddressBook("ss")
     contact_dict = {"first_name": "sunanda", "last_name": "shil", "age": 25, "mobile_no": 7578061886,
                     "email": "ssunanda02@gmail.com", "state": "Assam", "pin_no": 784115}
     contact = Contact(contact_dict)
     address_book.add_person(contact)
-    assert len(address_book.address_list) > 0
+    assert len(address_book.contact_list) > 0
 
 
 def test_edit_details():
     """
     testing the edit details method
     """
-    address_book = AddressBook()
+    address_book = AddressBook("ss")
     contact_dict = {"first_name": "sunanda", "last_name": "shil", "age": 25, "mobile_no": 7578061886,
                     "email": "ssunanda02@gmail.com", "state": "Assam", "pin_no": 784115}
     contact = Contact(contact_dict)
     address_book.add_person(contact)
-    contact.first_name = "sunanda"
-    first_name = "rahul"
-    contact.first_name = first_name
-    assert contact.first_name == "rahul"
+    address_book.edit_contact("sunanda",1,"rahul")
+    assert address_book.contact_list[0].first_name == "rahul"
 
 
 def test_delete_contact():
     """
     test delete contact method
     """
-    address_book = AddressBook()
+    address_book = AddressBook("ss")
     contact_dict = {"first_name": "sunanda", "last_name": "shil", "age": 25, "mobile_no": 7578061886,
                     "email": "ssunanda02@gmail.com", "state": "Assam", "pin_no": 784115}
     contact = Contact(contact_dict)
     address_book.add_person(contact)
-    contact.first_name = "sunanda"
-    first_name = "sunanda"
-    if contact.first_name == first_name:
-        address_book.address_list.remove(contact)
-    assert len(address_book.address_list) == 0
+    address_book.delete_contact("sunanda")
+    assert len(address_book.contact_list) == 0
+
+
+def test_get_adress_book():
+    """
+    testing the get address book method
+    """
+    address_book = AddressBook("ss")
+    assert isinstance(address_book, AddressBook) == True
